30e54a295fa0a6bdede3a63d9384a4287d0f3ea8
Thaqi Ul Islam Kafi <thaqiulislamkafi@tuta.io>
2026-07-31 22:58:38 +0600

    Theme Providers and QueryProvoders is implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "@/providers/QueryProvider";
+import ThemeProviders from "@/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>
+          <ThemeProviders>
+            {children}
+          </ThemeProviders>
+        </QueryProvider>
+      </body>
     </html>
   );
 }
